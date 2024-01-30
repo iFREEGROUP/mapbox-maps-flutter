eee4580dd41d35da5d42221c0fdc5a29c025f031
lantah <lsxt10@qq.com>
2024-01-30 15:06:14 +0800

    feat: add  snapshot script

---
+++ pigeon_generate/snapshot/run.sh
@@ -0,0 +1,5 @@
+flutter pub run pigeon \
+  --input pigeon_generate/snapshot/snapshot_manager.dart \
+  --dart_out pigeon_generate/snapshot/output/snapshot_manager.g.dart \
+  --swift_out pigeon_generate/snapshot/output/snapshot_manager.g.swift \
+  --java_out pigeon_generate/snapshot/output/snapshot_manager.java
---
+++ pigeon_generate/snapshotter/run.sh
@@ -0,0 +1,6 @@
+flutter pub run pigeon \
+  --input pigeon_generate/snapshotter/snapshotter.dart \
+  --dart_out pigeon_generate/snapshotter/output/snapshotter.g.dart \
+  --swift_out pigeon_generate/snapshotter/output/snapshotter.g.swift \
+  --java_out pigeon_generate/snapshotter/output/FLTSnapshot.java \
+  --java_package "com.mapbox.maps.pigeons"
