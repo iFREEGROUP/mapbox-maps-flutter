7fff483c4d85ed21c67f4695c58b7b0886200554
lantah <lsxt10@qq.com>
2024-01-31 14:37:07 +0800

    feat: add snapshot script for ios

--- pigeon_generate/snapshot/run.sh
+++ pigeon_generate/snapshot/run.sh
@@ -1,5 +1,7 @@
 flutter pub run pigeon \
   --input pigeon_generate/snapshot/snapshot_manager.dart \
   --dart_out pigeon_generate/snapshot/output/snapshot_manager.g.dart \
-  --swift_out pigeon_generate/snapshot/output/snapshot_manager.g.swift \
+  --objc_header_out pigeon_generate/snapshot/output/snapshot_manager.g.h \
+  --objc_source_out pigeon_generate/snapshot/output/snapshot_manager.g.m \
+   --objc_prefix 'FLT' \
   --java_out pigeon_generate/snapshot/output/snapshot_manager.java
